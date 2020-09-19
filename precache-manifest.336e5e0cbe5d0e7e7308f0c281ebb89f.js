25451a31bea7988076bebb66af158b26747f6499
harens <harensdeveloper@gmail.com>
2020-09-19 20:35:24 +0100

    Updates

---
+++ precache-manifest.336e5e0cbe5d0e7e7308f0c281ebb89f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42871d5c51d056dd777e00eccf2968dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84b68c1cd723e75f5d8a",
+    "url": "/static/css/main.dc58974d.chunk.css"
+  },
+  {
+    "revision": "f618ace27add88a44488",
+    "url": "/static/js/2.41ba93ca.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.41ba93ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84b68c1cd723e75f5d8a",
+    "url": "/static/js/main.2b51c1ee.chunk.js"
+  },
+  {
+    "revision": "e5a0d36eda18c926cc2b",
+    "url": "/static/js/runtime-main.738bb735.js"
+  },
+  {
+    "revision": "f1693c08b04dc8328d39f2c1ad92bf76",
+    "url": "/static/media/github.f1693c08.png"
+  },
+  {
+    "revision": "5171e96937f50d4252c86c0919076673",
+    "url": "/static/media/logo.5171e969.png"
+  }
+]);
