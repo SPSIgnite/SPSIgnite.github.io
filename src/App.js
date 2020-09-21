0772494e027f957bd0862953264dbd20b9e8743e
harens <harensdeveloper@gmail.com>
2020-09-21 17:51:23 +0100

    Import Correction + Static Website
    Closes #1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
+import { Fade } from "react-awesome-reveal";
 import logo from './Images/logo.png';
-import Fade from "react-awesome-reveal";
 import './App.css';
 import github from "./Images/github.png";
 
 function App() {
   return (
     <div className="App">
-        <Fade delay={1500}>
+        <Fade delay={1500} direction={"left"}>
             <a target="_blank" rel="noopener noreferrer" href={"https://github.com/SPSIgnite"}>
             <img src={github} className="Github-logo" alt="github" />
             </a>
         </Fade>
       <header className="App-header">
-          <Fade cascade>
+          <Fade cascade direction={"left"}>
             <img src={logo} className="App-logo" alt="logo" />
 		        <h1>Ignite</h1>
                 <h3>Spark Change</h3>
